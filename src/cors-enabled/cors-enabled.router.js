1dec26405b0ff57fa72b16dc3f1343ebe776cf96
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-21 15:32:15 -0600

    practiced with cors

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,17 +1,25 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require('cors');
+
+// const corsDelete = cors({methods: "DELETE"});
+//router.use(cors())
 
 router
   .route("/:corsId")
+  //.all(cors())
   .get(controller.read)
   .put(controller.update)
+  // .delete(corsDelete, controller.delete)
   .delete(controller.delete)
+  //.options(corsDelete)
   .all(methodNotAllowed);
 
 router
   .route("/")
-  .get(controller.list)
+  //.get(controller.list)
+  .get(cors(), controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
 
